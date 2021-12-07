f9ca6d531651977af9911a791e573141c01bac61
elianiva <dicha.arkana03@gmail.com>
2021-12-07 09:15:31 +0700

    feat: initial setup

---
+++ components/nav.js
@@ -0,0 +1,19 @@
+import styles from "../styles/nav.module.css";
+
+export default function Nav({ pages, active }) {
+  return (
+    <nav className={styles.box__nav}>
+      {pages.map(({ name, route }, idx) => (
+        <div
+          key={idx}
+          className={[
+            styles.nav__item,
+            active === route && styles.nav__item_active,
+          ].join(" ")}
+        >
+          {name}
+        </div>
+      ))}
+    </nav>
+  );
+}
