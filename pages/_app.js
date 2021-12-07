48042f3fd1c0242ee4c5b5e57ce5f7f858b6568f
elianiva <dicha.arkana03@gmail.com>
2021-12-07 09:20:26 +0700

    refactor: move to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useRouter } from "next/router";
+import Nav from "../components/nav.js";
+import styles from "../styles/index.module.css";
 import "../styles/globals.css";
 
+const pages = [
+  { name: "Total", route: "/" },
+  { name: "Users", route: "/users" },
+  { name: "Hourly", route: "/hourly" },
+  { name: "Calendar", route: "/calendar" },
+];
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.backdrop}></div>
+      <div className={styles.box}>
+        <Nav pages={pages} active={router.asPath} />
+        <div className={styles.box__content}>
+          <Component {...pageProps} />;
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,3 @@
-import { useRouter } from "next/router";
-import Nav from "../components/nav.js";
-import styles from "../styles/index.module.css";
-
-const pages = [
-  { name: "Total", route: "/" },
-  { name: "Users", route: "/users" },
-  { name: "Hourly", route: "/hourly" },
-  { name: "Calendar", route: "/calendar" },
-];
-
 export default function Home() {
-  const router = useRouter();
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.backdrop}></div>
-      <div className={styles.box}>
-        <Nav pages={pages} active={router.asPath} />
-        <div className={styles.box__content}>
-          <h1>Hello World</h1>
-        </div>
-      </div>
-    </div>
-  );
+  return <h1>92347</h1>;
 }
